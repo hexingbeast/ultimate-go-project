fccc05b673024e50107ffb122361f0f413aa35ad
aleksei_isaev <sherlockwulfs@gmail.com>
2025-02-17 11:04:48 +0400

    updated go version and added config parser

--- cmd/ultimate-go-project/main.go
+++ cmd/ultimate-go-project/main.go
@@ -1,6 +1,13 @@
 package main
 
-func main() {
+import (
+	"log/slog"
+	"ultimate-go-project/internal/config"
+)
 
+
+func main() {
+    cfg := config.MustLoad();
+    slog.Info("конфигурация загружена", "config", slog.Any("cfg", cfg))
 }
 
---
+++ internal/config/config.go
@@ -0,0 +1,45 @@
+package config
+
+import (
+	"log"
+	"os"
+	"time"
+
+	"github.com/spf13/viper"
+)
+type Config struct {
+    Env string `mapstructure:"env"`
+    StoragePath string `mapstructure:"storage_path"`
+    HTTPServer `mapstructure:"http_server"`
+}
+
+type HTTPServer struct {
+    Address string `mapstructure:"address"`
+    Timeout time.Duration `mapstructure:"timeout"`
+    IdleTimeout time.Duration `mapstructure:"idle_timeout"`
+    User string `mapstructure:"user"`
+    Password string `mapstructure:"password"`
+}
+
+func MustLoad() *Config {
+    configPath := os.Getenv("CONFIG_PATH")
+    if configPath == "" {
+        log.Fatal("CONFIG_PATH is not set")
+    }
+
+    viper.SetConfigFile(configPath)
+    viper.SetConfigType("yaml")
+    viper.AutomaticEnv()
+    
+    if err := viper.ReadInConfig(); err != nil {
+        log.Fatalf("Ошибка чтения конфигурации: %v", err)
+    }
+
+    var cfg Config
+    if err := viper.Unmarshal(&cfg); err != nil {
+        log.Fatalf("Ошибка при парсинге yaml: %v", err)
+    } 
+
+    return &cfg
+}
+
